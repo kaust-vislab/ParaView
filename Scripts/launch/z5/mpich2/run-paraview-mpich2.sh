8673e388cb58f648dc0eed7bbb699726fe0648da
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2015-05-17 09:04:53 +0300

    Fixed hostnames for z5 mpich script

--- Scripts/launch/z5/mpich2/run-paraview-mpich2.sh
+++ Scripts/launch/z5/mpich2/run-paraview-mpich2.sh
@@ -5,25 +5,25 @@
 module load kvl-remote
 module load paraview/4.3.1
 
-nodes=16
-tasks_per_node=1
+nodes=10
+tasks_per_node=8
 total_processes=$[nodes*tasks_per_node]
 
 # Define the address of your ParaView client workstation
 # madhu-00 desktop
-client=109.171.139.1
-port=$1
+#client=109.171.139.1
+#port=$1
 # madhu-01 desktop
 #client=109.171.139.2
-#client=109.171.138.145
-#port=$1
+client=109.171.138.145
+port=$1
 
 
 # Run pvserver with mpich2
 mpirun -genvlist LD_LIBRARY_PATH,PATH,PYTHONPATH \
     -genv MV2_ENABLE_AFFINITY 0 -genv IPATH_NO_CPUAFFINITY 1 \
     -np $total_processes \
-    -hosts gpgpu-00,gpgpu-01,gpgpu-02,gpgpu-03,gpgpu-04,gpgpu-05,gpgpu-06,gpgpu-07,gpgpu-08,gpgpu-09,gpgpu-10,gpgpu-11,gpgpu-12,gpgpu-13,gpgpu-14,gpgpu-15 \
+    -hosts z5-0-0,z5-0-1,z5-0-2,z5-1-0,z5-1-1,z5-1-2,z5-2-0,z5-2-1,z5-2-2,z5-3-0 \
     -ppn $tasks_per_node \
     /var/remote/software/paraview/4.3.1/bin/pvserver -display :0.0 \
     --use-offscreen-rendering \
