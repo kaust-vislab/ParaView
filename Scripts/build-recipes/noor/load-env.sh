0db1c8ad54e86a343adb310c33a11f948a74a5cb
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2015-06-03 15:35:49 +0300

    Added noor build scripts

---
+++ Scripts/build-recipes/noor/load-env.sh
@@ -0,0 +1,9 @@
+module load compilers-extra
+module load libs-extra
+
+module load gcc/4.9.1
+module load cmake/3.0.2/gcc-4.9.1
+module load python/2.7.8
+module load boost/1.56.0
+module load openmpi/1.8.3/gcc-4.9.1
+module load cuda/6.5
